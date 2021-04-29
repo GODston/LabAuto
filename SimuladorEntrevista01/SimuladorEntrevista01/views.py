c6cdf38759cb3346b34b9c30fab914f0d22cb141
Julian Tovar <julian.tovar360@gmail.com>
2021-04-28 22:48:40 -0500

    Se agregan recursos y pantallas para la aplicacion

--- SimuladorEntrevista01/SimuladorEntrevista01/views.py
+++ SimuladorEntrevista01/SimuladorEntrevista01/views.py
@@ -21,3 +21,12 @@ def index(request):
     return render(request, 'index.html', {
         "name_Emp": name_Emp
     })
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def about(request):
+    return render(request, 'about.html')
